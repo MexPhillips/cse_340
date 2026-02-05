6aa8477318a6624311fc779f36449af908763cd4
MexPhillips <kalubaphillip00@gmail.com>
2026-02-05 16:18:54 +0300

    keep on

--- routes/static.js
+++ routes/static.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
 
 // Static Routes
-// Serve the project's public folder reliably regardless of cwd
-const publicDir = path.join(__dirname, '..', 'public');
-router.use(express.static(publicDir));
-
-// Provide explicit mounts for convenience
-router.use('/css', express.static(path.join(publicDir, 'css')));
-router.use('/js', express.static(path.join(publicDir, 'js')));
-router.use('/images', express.static(path.join(publicDir, 'images')));
+// Set up "public" folder / subfolders for static files
+router.use(express.static("public"));
+router.use("/css", express.static(__dirname + "public/css"));
+router.use("/js", express.static(__dirname + "public/js"));
+router.use("/images", express.static(__dirname + "public/images"));
 
 module.exports = router;
 
